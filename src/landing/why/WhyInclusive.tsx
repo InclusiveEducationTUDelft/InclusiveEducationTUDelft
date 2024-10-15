d4bcf008249f4b770c8ba52efbc205433cbfb452
Shirley de Wit <SdeWit@users.noreply.github.com>
2024-10-15 13:41:59 +0200

    :speech_balloon: Updating text on landing page

--- src/landing/why/WhyInclusive.tsx
+++ src/landing/why/WhyInclusive.tsx
@@ -11,13 +11,13 @@ const infoCards = [
   },
   {
     title: "Gender",
-    content: "Women and nonbinary people are underrepresented in most STEM field. Providing an inclusive and welcoming environment for all is a crucial step towards inclusivity.",
+    content: "Women and nonbinary people are underrepresented in most STEM field. Providing an inclusive and welcoming environment for all is a crucial step towards gender diversity in STEM.",
     icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/f36c5f32a5c415a6ee76b79b280073c9dd96d610269e88972c4fd4d84e432e99?apiKey=91547a0531724dfdbec3493d0aeb27e5&",
     href: "#gender"
   },
   {
     title: "Neurodiversity",
-    content: "Students experiencing and interacting with the world around them in many different ways. Next to providing the necessary facilities, we need to be open and considerate to foster a safe and inclusive environment.",
+    content: "Students experiencing and interacting with the world around them in many different ways. We can support all of our students in their learning through inclusive education.",
     icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/72d23130ec66bbe8f014bf49ed685c4b7ba08a06782039eb59e091c82c124b26?apiKey=91547a0531724dfdbec3493d0aeb27e5&",
     href: "#neurodiversity"
   }
@@ -31,9 +31,9 @@ const WhyInclusive: React.FC = () => {
         As higher educational institutions shift their focus towards enhancing educational outcomes for all students, 
         regardless of their differences, it is essential to create content that is suitable for all ​(Dewsbury, 2019)​. 
         Recognizing and addressing the unique needs of diverse student populations is crucial in fostering an inclusive 
-        environment. The sense of belonging of students can directly impact their results and performance and as an 
-        institution, the aim is to improve quality of education. ​(Dewsbury, 2019)​. Besides this, there is evidence to 
-        prove that the higher dropout rate is linked to students being underrepresented in STEM fields. ​(Dewsbury, 2019)​.    
+        environment. Inclusive STEM education will help a diverse group of students to progress through their studies. 
+        This results in more people being educated in STEM fields, providing them with good career opportunities. 
+        Moreover, since STEM innovations largely impact society, we enable a better representation of society to create, build and shape the direction of our fields. 
       </p>
       <div className={styles.cardContainer}>
         {infoCards.map((card, index) => (

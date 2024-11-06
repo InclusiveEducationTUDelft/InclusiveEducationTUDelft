f2d12bbf778e5dc4e5769510b7b888ad78181d3b
Shirley de Wit <SdeWit@users.noreply.github.com>
2024-11-06 10:48:25 +0100

    :sparkles: overview of content

--- src/gender/Gender.tsx
+++ src/gender/Gender.tsx
@@ -16,6 +16,72 @@ function Gender() {
         <section>
             <PageExplanation title={"Gender"} innerHtml={text_intro} image={"gender-hero.png"} />
         </section>
+
+        <section
+            style={{
+                width: "80%",
+                marginLeft: "auto",
+                marginRight: "auto",
+            }}>
+            <h2>Concept of Gender</h2>
+            <div>
+            Before diving into the topic, let's discuss the concept of gender.
+            - different elements, gender identity using the  <a href="https://www.itspronouncedmetrosexual.com/2018/10/the-genderbread-person-v4/">genderbread person</a> by Sam Killermann.
+            - resource on gender beyond binary
+            - althoug gender is more than men/women most resources/research describes binary gender 
+            </div>
+            <img src="https://www.itspronouncedmetrosexual.com/wp-content/uploads/2018/10/Genderbread-Person-v4-1200.png" alt="genderbread" width="500" c></img>
+        </section>
+
+        <section
+            style={{
+                width: "80%",
+                marginLeft: "auto",
+                marginRight: "auto",
+            }}>
+            <h2>Why is it import to increase gender diversity in STEM fields?</h2>
+            <div>
+            - Current numbers
+            - We need more people to fill the increasing number of vacancies. 
+            - We want people to participate in fields with good career opportunities, benefiting gender equality. 
+            - Moreover, gender diversity benefits productivity, innovations and reduces the risk of biased products and innovations.
+            - Examples of these innovations
+            </div>
+        </section>
+
+        <section
+            style={{
+                width: "80%",
+                marginLeft: "auto",
+                marginRight: "auto",
+            }}>
+            <h2>Factors influencing the gender gap</h2>
+            <div>
+            - see slides
+            - Jeffrey Maloy, Monika B. Kwapisz, and Bryce E. Hughes. 2022. Factors Influencing Retention of Transgender and Gender Nonconforming Students in Undergraduate STEM Majors. CBE—Life Sciences Education 21, 1 (March 2022), ar13. 
+            https://doi.org/10.1187/cbe.21-05-0136
+            - Amanda Menier, Rebecca Zarch, and Stacey Sexton. 2021. Broadening Gender in Computing for Transgender and Nonbinary Learners. In 2021 Conference on Research in Equitable and Sustained Participation in Engineering, Computing, and Technology (RESPECT). IEEE, Philadelphia, PA, USA, 1–5. 
+            https://doi.org/10.1109/RESPECT51740.2021.9620612
+            - Shirley: look for source on inclusive langauage (as suggested by reviewer)
+            </div>
+        </section>
+
+        <section
+            style={{
+                width: "80%",
+                marginLeft: "auto",
+                marginRight: "auto",
+            }}>
+            <h2>Solutions</h2>
+            <div>
+            - since the problem is complex, the solution is also at multiple places/stakeholders
+            - but you can make steps
+            - focus on making the systems inclusive 
+            - scope down to higher education
+            - a couple of examples/tips with referece to the example/tip page
+            </div>
+        </section>
+
         <Footer />
     </>
 }

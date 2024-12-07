5dbca23da746b5ba0da63ef5c800bd364ce48fa0
Gargant <37416030+Gargant0373@users.noreply.github.com>
2024-12-07 19:27:42 +0100

    Progress on Gender page.

--- src/gender/Gender.tsx
+++ src/gender/Gender.tsx
@@ -1,8 +1,9 @@
 import Footer from "../misc/footer/Footer";
 import Header from "../misc/header/Header";
 import PageExplanation from "../misc/page-explanation/PageExplanation";
+import styles from "./Gender.module.css";
 
-const text_intro = "We have a gender gap in many STEM fields. Increasing gender diversity benefits the labor market, gender equality, and innovations. The lack of gender diversity is caused by, among others, a low sense of belonging, self-efficacy and stereotypes. Although it is a complex problem, we can still do several things in our education to attract and retain a diverse group of people. Below we will continue discussing the problem and possible solutions.";
+const text_intro = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publish";
 // const text_first = "Before diving into the topic, let's discuss the concept of gender."
 // const text_second = "Here we will explain the importance of the problem in more detail"
 // const examples = "To discuss, car door / dummy / apple health / chatGPT"
@@ -14,7 +15,22 @@ function Gender() {
     return <>
         <Header />
         <section>
-            <PageExplanation title={"Gender"} innerHtml={text_intro} image={"gender-hero.png"} />
+            <PageExplanation title={"What is gender?"} innerHtml={text_intro} image={"gender-hero.png"} />
+            <div className={styles.whyContainer}>
+                <div className={styles.whyContent}>
+                    <div className={styles.whyTitle}>Why is it important?</div>
+                    <div className={styles.whyTitle}></div>
+                </div>
+                <div className={styles.whyContent}>
+                    <img src="./gender2.png" alt="Why is it important?" className={styles.whyImage} />
+                    <div className={styles.whyText}>
+                        Let’s look at the current numbers at TU Delft  <br />
+                        • 32% of our PhD Students are women  <br />
+                        • 19% of our full professors are women <br />
+                        These numbers extend into the industry where 1 out of 5 women work in tech and 1 out of 2 men work in tech. We need more people to fill the increasing number of vacancies. We want people to participate in fields with good career opportunities, benefiting gender equality. Moreover, gender diversity benefits productivity, innovations and reduces the risk of biased products and innovations. For example, a woman is 47% more likely to be injured and 17% more likely to die in a car crash because crash tests are not done on with female dummies. (Perez, 2019). In 2022, the first female crash test dummy was designed.
+                    </div>
+                </div>
+            </div>
         </section>
         <Footer />
     </>

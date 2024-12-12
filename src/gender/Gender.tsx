8ee070df37f5430c4fc721ee6b80438d32731209
Gargant <37416030+Gargant0373@users.noreply.github.com>
2024-12-12 19:13:26 +0100

    More work on Gender page.

--- src/gender/Gender.tsx
+++ src/gender/Gender.tsx
@@ -1,6 +1,7 @@
 import Footer from "../misc/footer/Footer";
 import Header from "../misc/header/Header";
 import PageExplanation from "../misc/page-explanation/PageExplanation";
+import Factors from "./Factors";
 import styles from "./Gender.module.css";
 
 const text_intro = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publish";
@@ -31,6 +32,7 @@ function Gender() {
                     </div>
                 </div>
             </div>
+            <Factors />
         </section>
         <Footer />
     </>
